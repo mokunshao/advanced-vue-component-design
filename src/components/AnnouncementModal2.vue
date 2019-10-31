006c3805dd3ab4eb5d187e2a27f23faf2dc6abdd
mokunshao <mokunshao@foxmail.com>
2019-10-31 18:01:48 +0800

    update ep4

---
+++ src/components/AnnouncementModal2.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="modal-backdrop" v-show="show">
+    <div class="modal">
+      <div>
+        通知
+      </div>
+      <button @click="dismiss">close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+    },
+  },
+  name: 'announcement-modal-2',
+  methods: {
+    dismiss() {
+      this.$emit('close');
+    },
+  },
+  created() {
+    this.EscapeHandler = (e) => {
+      if (e.key === 'Escape' && this.show) {
+        this.dismiss();
+      }
+    };
+    document.addEventListener('keydown', this.EscapeHandler);
+  },
+  destroyed() {
+    document.removeEventListener('keydown', this.EscapeHandler);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  padding: 1em;
+  font-size: 1em;
+
+  &-backdrop {
+    background-color: rgba($color: #000000, $alpha: 0.5);
+    left: 0;
+    top: 0;
+    position: fixed;
+    height: 100%;
+    width: 100%;
+  }
+}
+</style>
--- src/views/ep4.vue
+++ src/views/ep4.vue
@@ -1,22 +1,34 @@
 <template>
   <div>
     <button @click="modalOpen = true">Open</button>
+    <button @click="modalOpen2 = true">Open2</button>
+    <button @click="modalOpen3 = true">Open3</button>
     <announcement-modal :show="modalOpen" @close="modalOpen = false"
       >modal</announcement-modal
+    >
+    <announcement-modal-2 :show="modalOpen2" @close="modalOpen2 = false"
+      >modal</announcement-modal-2
+    >
+    <announcement-modal-3 :show="modalOpen3" @close="modalOpen3 = false"
+      >modal</announcement-modal-3
     >
   </div>
 </template>
 
 <script>
 import AnnouncementModal from '../components/AnnouncementModal.vue';
+import AnnouncementModal2 from '../components/AnnouncementModal2.vue';
+import AnnouncementModal3 from '../components/AnnouncementModal3.vue';
 
 export default {
   components: {
     AnnouncementModal,
+    AnnouncementModal2,
+    AnnouncementModal3,
   },
   name: 'ep4',
   data() {
-    return { modalOpen: false };
+    return { modalOpen: false, modalOpen2: false, modalOpen3: false };
   },
 };
 </script>
